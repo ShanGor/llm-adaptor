b29cf18ef509d4e5b7d998865f1fea40040660b1
Samuel Chan <emschan@live.cn>
2025-03-14 11:43:37 +0800

    Initial Version

---
+++ src/main/java/io/github/shangor/llm/pojo/OpenAiCompletionRequest.java
@@ -0,0 +1,41 @@
+package io.github.shangor.llm.pojo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class OpenAiCompletionRequest {
+    private String model;
+    private List<? extends RequestMessage> messages;
+    private Integer max_completion_tokens;
+    private Boolean logprobs = false;
+    private Boolean stream = true;
+    private Double temperature = 1.0;
+    private Map<String, Object> stream_options = Map.of("include_usage", true);
+
+    @Data
+    public static class RequestMessage {
+        protected String role;
+    }
+
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    public static class RequestMessageText extends RequestMessage {
+        private String content;
+    }
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    public static class RequestMessageImage extends RequestMessage {
+        private List<Content> content;
+    }
+
+    @Data
+    public static class Content {
+        private String type;
+        private String text;
+        private String image_url;
+    }
+}
